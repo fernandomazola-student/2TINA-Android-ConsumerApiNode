825898f2a4ba16a198b4c53518d634a8439691f4
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-19 11:15:06 -0300

    Implementado os metodos response e failure do metodo GET

--- app/src/main/java/br/com/fiap/a2tina_android_consumerapinode/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_consumerapinode/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.fiap.a2tina_android_consumerapinode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -20,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         RetrofitDadosInterface api = retrofit.create(RetrofitDadosInterface.class);
+        api.getDados().enqueue(new Callback<Contato>() {
+            @Override
+            public void onResponse(Call<Contato> call, Response<Contato> response) {
+                
+            }
+
+            @Override
+            public void onFailure(Call<Contato> call, Throwable t) {
+
+            }
+        });
     }
 
 }
